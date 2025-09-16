7164385c0ac81b05325732b4ac20f8867655b4b5
erfan-ne <erfan.neshati@gmail.com>
2025-09-16 13:48:42 +0330

    assign colors to notes

--- src/components/NoteApp/Note.js
+++ src/components/NoteApp/Note.js
@@ -4,7 +4,7 @@ export default class Note extends Component {
 
     render() {
         return (
-            <div className="card shadow-sm rounded" style={{ backgroundColor: '#fff' }}><p className="card-text p-3">{this.props.title}</p></div>
+            <div className="card shadow-sm rounded" style={{ backgroundColor: this.props.color }}><p className="card-text p-3">{this.props.title}</p></div>
         )
     }
 }
